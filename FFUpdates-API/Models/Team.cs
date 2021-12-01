75f06d56a1ac1993e3b4f5f6c6502766d563d650
Tyler Evers <TEves2015@gmail.com>
2021-11-30 23:00:29 -0600

    Cleaned up usings following implementation of Player controller, model and repositories

--- FFUpdates-API/Models/Team.cs
+++ FFUpdates-API/Models/Team.cs
@@ -1,9 +1,4 @@
-﻿using FFUpdates_API.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace FFUpdates_API.Models
 {
     public class Team
--- FFUpdates-API/Repositories/Team/ITeamRepository.cs
+++ FFUpdates-API/Repositories/Team/ITeamRepository.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FFUpdates_API.Models;
 
@@ -8,12 +6,12 @@ namespace FFUpdates_API.Repositories
 {
     public interface ITeamRepository
     {
-        Task<List<Models.Team>> Get();
+        Task<List<Team>> Get();
 
-        Task<Models.Team> GetById(int id);
+        Task<Team> GetById(int id);
 
-        Task<List<Models.Team>> GetFantasyTeams();
+        Task<List<Team>> GetFantasyTeams();
 
-        Task<List<Models.Team>> GetNFLTeams();
+        Task<List<Team>> GetNFLTeams();
     }
 }
